874cc0c67c5c585faa8ef30a73c3bda84ad2e3d6
conradkoh <conradkoh@gmail.com>
2022-03-17 00:10:38 +0800

    Added compare logic

--- pages/compare.tsx
+++ pages/compare.tsx
@@ -1,13 +1,116 @@
 import { MainLayout } from '@client/components/layouts/MainLayout';
 import SideNav from '@client/components/layouts/SideNav/SideNav';
-import React from 'react';
+import ComparePageTemplate from '@client/components/templates/ComparePageTemplate';
+import React, { useCallback, useEffect, useState } from 'react';
 import { FunctionComponent } from 'react';
 interface ComparePageProps {}
+const PERSIST_KEY_LEFT_VALUE = 'compare-left-value';
+const PERSIST_KEY_RIGHT_VALUE = 'compare-right-value';
 const ComparePage: FunctionComponent<ComparePageProps> = (props) => {
+  const [left, setLeft] = useState({
+    value: '',
+  });
+  const [right, setRight] = useState({
+    value: '',
+  });
+  useEffect(() => {
+    setLeft({ value: localStorage?.getItem?.(PERSIST_KEY_LEFT_VALUE) || '' });
+  }, []);
+
+  useEffect(() => {
+    setRight({ value: localStorage?.getItem?.(PERSIST_KEY_RIGHT_VALUE) || '' });
+  }, []);
+  const [output, setOutput] = useState({ value: '' });
+  const handleLeftChange = useCallback((value) => {
+    setLeft({ value });
+  }, []);
+  const handleRightChange = useCallback((value) => {
+    setRight({ value });
+  }, []);
+
+  //persist state
+  useEffect(() => {
+    localStorage.setItem(PERSIST_KEY_LEFT_VALUE, left.value);
+    localStorage.setItem(PERSIST_KEY_RIGHT_VALUE, right.value);
+  }, [left.value, right.value]);
+
+  const updateConsoleOutput = useCallback(() => {
+    let op = '';
+    compareLines({
+      log: (v) => {
+        op += `${v}\n`;
+      },
+    })(left.value, right.value);
+    setOutput({ value: op });
+  }, [left, right]);
+
+  //rerun compute whenever console output can be generated
+  useEffect(() => {
+    updateConsoleOutput();
+  }, [updateConsoleOutput]);
   return (
     <MainLayout title={'Compare'}>
-      <div>Compare</div>
+      <ComparePageTemplate
+        left={left}
+        right={right}
+        output={output}
+        onLeftChange={handleLeftChange}
+        onRightChange={handleRightChange}
+      />
     </MainLayout>
   );
 };
+
+const compareLines =
+  (deps: { log: (val: string) => void }) => (left: string, right: string) => {
+    const { log } = deps;
+    if (!left.trim()) {
+      log(`Waiting for input on left...`);
+      return;
+    }
+    if (!right.trim()) {
+      log(`Waiting for input on right...`);
+      return;
+    }
+    let left_lines = left
+      .split('\n')
+      .map((line) => line.trim())
+      .filter((i) => i);
+    let right_lines = right
+      .split('\n')
+      .map((line) => line.trim())
+      .filter((i) => i);
+    let left_index = left_lines.reduce<Record<string, { name: string }>>(
+      (state, cur, idx) => {
+        state[cur] = { name: cur };
+        return state;
+      },
+      {}
+    );
+    let right_index = right_lines.reduce<Record<string, { name: string }>>(
+      (state, cur, idx) => {
+        state[cur] = { name: cur };
+        return state;
+      },
+      {}
+    );
+
+    //Find left items missing in right
+    for (let name of left_lines) {
+      let found = right_index[name] ? true : false;
+      if (!found) {
+        log(`${name} found in left but not found in right.`);
+      }
+    }
+
+    log('===================================');
+
+    //Find right items missing in left
+    for (let name of right_lines) {
+      let found = left_index[name] ? true : false;
+      if (!found) {
+        log(`${name} found in right but not found in left.`);
+      }
+    }
+  };
 export default ComparePage;
--- src/client/components/templates/ComparePageTemplate.tsx
+++ src/client/components/templates/ComparePageTemplate.tsx
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { FunctionComponent } from 'react';
-interface ComparePageTemplateProps {}
-const ComparePageTemplate: FunctionComponent<ComparePageTemplateProps> = (
-  props
-) => {
-  return <div>Compare</div>;
+import styles from './ComparePageTemplate.module.scss';
+interface ComparePageTemplateProps {
+  left: { value: string };
+  right: { value: string };
+  output: { value: string };
+  onLeftChange: (newVal: string) => void;
+  onRightChange: (newVal: string) => void;
+}
+const ComparePageTemplate: FunctionComponent<ComparePageTemplateProps> = ({
+  left,
+  right,
+  output,
+  onLeftChange,
+  onRightChange,
+}) => {
+  const handleLeftChange = useCallback(
+    (e) => {
+      onLeftChange(e.target.value);
+    },
+    [onLeftChange]
+  );
+  const handleRightChange = useCallback(
+    (e) => {
+      onRightChange(e.target.value);
+    },
+    [onRightChange]
+  );
+  return (
+    <div className="px-3 py-3 w-100 h-full flex flex-col">
+      <div className="flex flex-row grow grid grid-cols-2 gap-3">
+        <div className="flex flex-col grow">
+          <div className="mt-3">Left:</div>
+          <textarea
+            value={left.value}
+            onChange={handleLeftChange}
+            className="grow bg-slate-100 w-full"
+          ></textarea>
+        </div>
+        <div className="flex flex-col grow">
+          <div className="mt-3">Right:</div>
+          <textarea
+            value={right.value}
+            onChange={handleRightChange}
+            className="grow bg-slate-100 w-full"
+          ></textarea>
+        </div>
+      </div>
+      <div className="mt-3">Output:</div>
+      <textarea
+        readOnly
+        value={output.value}
+        className={`mt-1 w-full bg-slate-200 ${styles.output}`}
+      ></textarea>
+    </div>
+  );
 };
 export default ComparePageTemplate;

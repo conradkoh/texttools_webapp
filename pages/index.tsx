d781c4da97f3e4163b1ae5de4dc50b25912b0b33
conradkoh <conradkoh@gmail.com>
2022-03-16 22:57:30 +0800

    Added basic styling and tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { MainLayout } from '@client/components/layouts/MainLayout';
 export default function Home() {
   return (
     <MainLayout title="Home">
+      <div>Side Nav</div>
       <div>Main Content</div>
     </MainLayout>
   );

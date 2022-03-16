6368d0a666fe0b23d18798de3865bb89453ebf81
conradkoh <conradkoh@gmail.com>
2022-03-16 22:16:27 +0800

    Bootstrap project

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import { MainLayout } from '@client/components/layouts/MainLayout';
+
+export default function Home() {
+  return (
+    <MainLayout title="Home">
+      <div>Main Content</div>
+    </MainLayout>
+  );
+}
---
+++ src/client/components/templates/ComparePageTemplate.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { FunctionComponent } from 'react';
+interface ComparePageTemplateProps {}
+const ComparePageTemplate: FunctionComponent<ComparePageTemplateProps> = (
+  props
+) => {
+  return <div>Compare</div>;
+};
+export default ComparePageTemplate;

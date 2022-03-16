7a66566e317909cf8c7ad26aa0743eaa917f30ef
conradkoh <conradkoh@gmail.com>
2022-03-17 00:25:54 +0800

    Added empty space filler

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,9 @@ import { MainLayout } from '@client/components/layouts/MainLayout';
 export default function Home() {
   return (
     <MainLayout title="Home">
-      <div>Main Content</div>
+      <div className="w-full h-full flex justify-center items-center">
+        <div className="text-9xl">✐</div>
+      </div>
     </MainLayout>
   );
 }
